c024d136f2f4f00b2a19b92880c26e754cf087d1
Arnau <arnau@ersilia.io>
2025-03-24 14:37:14 +0100

    uncommenting code blocks

--- scripts/01_fetch_pathogen_data_from_chembl.sh
+++ scripts/01_fetch_pathogen_data_from_chembl.sh
@@ -49,9 +49,9 @@ fi
 
 echo "Fetching data for pathogen $PATHOGEN_CODE and storing in $OUTPUT_DIR"
 
-# python $SCRIPT_DIR/../src/011_pathogen_getter.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR 
-# python $SCRIPT_DIR/../src/012_clean_fetched_pathogen_data.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
-# python $SCRIPT_DIR/../src/013_binarize_fetched_pathogen_data.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
-# python $SCRIPT_DIR/../src/014_datasets_modelability.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
-# python $SCRIPT_DIR/../src/015_select_tasks.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
+python $SCRIPT_DIR/../src/011_pathogen_getter.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR 
+python $SCRIPT_DIR/../src/012_clean_fetched_pathogen_data.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
+python $SCRIPT_DIR/../src/013_binarize_fetched_pathogen_data.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
+python $SCRIPT_DIR/../src/014_datasets_modelability.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
+python $SCRIPT_DIR/../src/015_select_tasks.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR
 python $SCRIPT_DIR/../src/016_wrapup_tasks_and_clean_output_folder.py --pathogen_code $PATHOGEN_CODE --output_dir $OUTPUT_DIR

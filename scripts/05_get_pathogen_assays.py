d378b71ef2f85123ffeda96472365d8a32dedc80
Arnau <arnau@ersilia.io>
2025-10-24 10:43:33 +0200

    getting assay info for all pathogens

--- scripts/05_get_pathogen_assays.py
+++ scripts/05_get_pathogen_assays.py
@@ -10,11 +10,15 @@ root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(root, "..", "src"))
 from default import CONFIGPATH, MIN_ASSAY_SIZE
 
+# Loading ChEMBL preprocessed data
+print("Loading ChEMBL preprocessed data...")
+ChEMBL = pd.read_csv(os.path.join(root, "..", "config", "chembl_processed", "activities_preprocessed.csv"), low_memory=False)
+print(f"Original size: {len(ChEMBL)}")
+
 # List of pathogens
 pathogens = ["Acinetobacter baumannii", "Candida albicans", "Campylobacter", "Escherichia coli", "Enterococcus faecium", "Enterobacter",
              "Helicobacter pylori", "Klebsiella pneumoniae", "Mycobacterium tuberculosis", "Neisseria gonorrhoeae", "Pseudomonas aeruginosa",
              "Plasmodium falciparum", "Staphylococcus aureus", "Schistosoma mansoni", "Streptococcus pneumoniae"]
-pathogens = ["Mycobacterium tuberculosis"]
 
 def get_pathogen_code(pathogen):
     return str(pathogen.split()[0][0] + pathogen.split()[1]).lower() if len(pathogen.split()) > 1 else pathogen.lower()
@@ -22,24 +26,19 @@ def get_pathogen_code(pathogen):
 # For each pathogen
 for pathogen in pathogens:
 
-    # Loading ChEMBL preprocessed data
-    print("Loading ChEMBL preprocessed data...")
-    ChEMBL = pd.read_csv(os.path.join(root, "..", "config", "chembl_processed", "activities_preprocessed.csv"), low_memory=False)
-    print(f"Original size: {len(ChEMBL)}")
-
     print(f"Filtering for pathogen: {pathogen}...")
     pathogen_code = get_pathogen_code(pathogen)
     PATH_TO_OUTPUT = os.path.join(root, "..", "output", pathogen_code)
     os.makedirs(PATH_TO_OUTPUT, exist_ok=True)
-    ChEMBL = ChEMBL[ChEMBL['target_organism'].str.contains(pathogen, case=False, na=False) | 
-                    ChEMBL['assay_organism'].str.contains(pathogen, case=False, na=False)].reset_index(drop=True)
+    ChEMBL_pathogen = ChEMBL[ChEMBL['target_organism'].str.contains(pathogen, case=False, na=False) | 
+                             ChEMBL['assay_organism'].str.contains(pathogen, case=False, na=False)].reset_index(drop=True)
 
-    print(f"Number of activities: {len(ChEMBL)}")
-    print(f"Number of unique compounds: {len(set(ChEMBL['compound_chembl_id']))}") 
-    df = dict(Counter(ChEMBL['target_organism']))
+    print(f"Number of activities: {len(ChEMBL_pathogen)}")
+    print(f"Number of unique compounds: {len(set(ChEMBL_pathogen['compound_chembl_id']))}") 
+    df = dict(Counter(ChEMBL_pathogen['target_organism']))
     df = pd.DataFrame([[i, df[i]] for i in sorted(df, key = lambda x: df[x], reverse=True)], columns=['organism', 'count'])
     df.to_csv(os.path.join(PATH_TO_OUTPUT, "target_organism_counts.csv"), index=False)
-    ChEMBL.to_csv(os.path.join(PATH_TO_OUTPUT, f"{pathogen_code}_ChEMBL_data.csv"), index=False)
+    ChEMBL_pathogen.to_csv(os.path.join(PATH_TO_OUTPUT, f"{pathogen_code}_ChEMBL_data.csv"), index=False)
 
     # Helper function - is there only a single value?
     def only_one(values, name):
@@ -48,7 +47,7 @@ for pathogen in pathogens:
         return values[0]
 
     # Get unique assays
-    assays = sorted(set(ChEMBL['assay_chembl_id']))
+    assays = sorted(set(ChEMBL_pathogen['assay_chembl_id']))
 
     ASSAYS_INFO = []
     print("Collecting individual assay information...")
@@ -57,7 +56,7 @@ for pathogen in pathogens:
     for assay in tqdm(assays):
 
         # Get subset of strain + assay data
-        df_ = ChEMBL[ChEMBL["assay_chembl_id"] == assay]
+        df_ = ChEMBL_pathogen[ChEMBL_pathogen["assay_chembl_id"] == assay]
         
         # Get values
         assay_type = list(set(df_['assay_type']))
